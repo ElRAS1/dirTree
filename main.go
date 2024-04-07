77557a3de68b639b7cd1cc47abb722db8855d103
Elmir <elmir.rasulov.2002@mail.ru>
2024-04-07 14:23:47 +0300

    print tree directory

---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	// "path/filepath"
+	// "strings"
+)
+
+type Config struct {
+	tabFile  string
+	tabDir   string
+	levelDir int
+	// levelFile int
+	prfile bool
+}
+
+func main() {
+	out := os.Stdout
+	// if !(len(os.Args) == 2 || len(os.Args) == 3) {
+	// 	panic("usage go run main.go . [-f]")
+	// }
+	// path := os.Args[1]
+	// printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+
+	// err := dirTree(out, path, printFiles)
+
+	err := dirTree(out, "testdata", true)
+
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func dirTree(out io.Writer, path string, printFiles bool) error {
+
+	config := Config{
+		// tabFile:  "\t├───",
+		tabDir:   "├───",
+		levelDir: 0,
+		// levelFile: 1,
+		prfile: printFiles,
+	}
+
+	dirArr := make([]string, 0)
+	dirFile := make([]string, 0)
+	tree(path, &config, &dirArr, &dirFile)
+
+	// fmt.Println(dirArr)
+	// fmt.Println(dirFile)
+	return nil
+}
+
+func tree(path string, config *Config, dirArr *[]string, dirFile *[]string) {
+
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return
+	}
+
+	for _, i := range entries {
+		if i.IsDir() {
+			*dirArr = append(*dirArr, i.Name())
+			printDir(dirArr, config)
+			config.levelDir++
+			*dirArr = make([]string, 0)
+			tree(path+"/"+i.Name(), config, dirArr, dirFile)
+
+		} else if config.prfile {
+			*dirFile = append(*dirFile, i.Name())
+			printFile(dirFile, config)
+			*dirFile = make([]string, 0)
+
+		}
+
+	}
+
+	config.levelDir = 0
+
+}
+
+// сделать одну функцию которая будет принимать разделитель
+func printDir(dirArr *[]string, config *Config) {
+
+	for indx, i := range *dirArr {
+		if indx == len(*dirArr)-1 && config.levelDir > 0 {
+			fmt.Printf("%s└───%s\n", strings.Repeat("    ", config.levelDir), i)
+		} else {
+			fmt.Printf("%s%s%s\n", strings.Repeat("    ", config.levelDir), config.tabDir, i)
+		}
+	}
+}
+
+func printFile(dirFile *[]string, config *Config) {
+
+	for indx, i := range *dirFile {
+		if indx == len(*dirFile)-1 {
+			fmt.Printf("%s└───%s\n", strings.Repeat("    ", config.levelDir), i)
+		} else {
+			fmt.Printf("%s%s%s\n", strings.Repeat("    ", config.levelDir), config.tabDir, i)
+		}
+	}
+
+}
